fc13866f8dd4b6d4ddebe06dca5403ddff5f1cd4
石凯 <k1330942316@163.com>
2022-01-07 14:09:39 +0800

    配置文件

--- dao/redis/redis.go
+++ dao/redis/redis.go
@@ -3,19 +3,19 @@ package redis
 import (
 	"fmt"
 	"github.com/go-redis/redis"
-	"github.com/spf13/viper"
+	"myApp/settings"
 )
 
 var rdb *redis.Client
 
-func Init() (err error) {
+func Init(cfg *settings.RedisConfig) (err error) {
 	rdb = redis.NewClient(&redis.Options{
 		Addr: fmt.Sprintf("%s:%d",
-			viper.GetString("redis.host"),
-			viper.GetInt("redis.port")),
-		Password: viper.GetString("redis.password"),
-		DB:       viper.GetInt("redis.db"),
-		PoolSize: viper.GetInt("redis.pool_size"),
+			cfg.Host,
+			cfg.Port),
+		Password: cfg.Password,
+		DB:       cfg.DB,
+		PoolSize: cfg.PoolSize,
 	})
 
 	_, err = rdb.Ping().Result()
--- settings/settings.go
+++ settings/settings.go
@@ -7,19 +7,62 @@ import (
 	"github.com/spf13/viper"
 )
 
+var Conf = new(AppConfig)
+
+type AppConfig struct {
+	Name    string `mapstructure:"name"`
+	Mode    string `mapstructure:"mode"`
+	Version string `mapstructure:"version"`
+	Port    int    `mapstructure:"port"`
+
+	*MySQLConfig `mapstructure:"mysql"`
+	*LogConfig   `mapstructure:"log"`
+	*RedisConfig `mapstructure:"redis"`
+}
+
+type MySQLConfig struct {
+	Host         string `mapstructure:"host"`
+	User         string `mapstructure:"user"`
+	Password     string `mapstructure:"password"`
+	Dbname       string `mapstructure:"dbname"`
+	Port         int    `mapstructure:"port"`
+	MaxOpenConns int    `mapstructure:"max_open_conns"`
+	MaxIdleConns int    `mapstructure:"max_idle_conns"`
+}
+type LogConfig struct {
+	Level      string `mapstructure:"level"`
+	Filename   string `mapstructure:"filename"`
+	MaxSize    int    `mapstructure:"max_size"`
+	MaxAge     int    `mapstructure:"max_age"`
+	MaxBackups int    `mapstructure:"max_backups"`
+}
+type RedisConfig struct {
+	Host     string `mapstructure:"host"`
+	Password string `mapstructure:"password"`
+	Port     int    `mapstructure:"port"`
+	DB       int    `mapstructure:"db"`
+	PoolSize int    `mapstructure:"pool_size"`
+}
+
 func Init() (err error) {
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
+	viper.SetConfigFile("config.json")
+	//viper.SetConfigName("config")
+	//viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
 	err = viper.ReadInConfig()
 	if err != nil {
 		fmt.Printf("viper.ReadInConfig() failed err: %v \n", err)
 		return
 	}
-
+	if err := viper.Unmarshal(Conf); err != nil {
+		fmt.Printf("viper.Unmarshal failed, err:%v\n", err)
+	}
 	viper.WatchConfig()
 	viper.OnConfigChange(func(in fsnotify.Event) {
 		fmt.Println("配置文件修改...")
+		if err := viper.Unmarshal(Conf); err != nil {
+			fmt.Printf("viper.Unmarshal failed, err:%v\n", err)
+		}
 	})
 	return
 }
